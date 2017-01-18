0dcc7bd57f340442dae5c9bbaa76f43abd27d323
Margreet <0margreet@gmail.com>
2017-01-18 13:54:04 +0100

    Add link to twitter authentication

---
+++ credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+  cookieSecret : 'my_secret_abc_1234',
+  secret: 'my_secret_abc_1234',
+  cookie: {maxAge : 6000},
+  twitter: {
+    consumerKey: "MeE3PURpIoftGnExeEdPWSZpx",
+    consumerSecret: "7yczPajHTg5t92xXhEsZFe6rSQ56c211Yk02jaLW1g0XXlymLb",
+    callbackURL: "http://127.0.0.1:80/login-twitter"
+  }
+};
